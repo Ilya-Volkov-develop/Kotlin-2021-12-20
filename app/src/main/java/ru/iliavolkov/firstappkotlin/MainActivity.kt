c5fa4a8bdaa8511890bb117fc097f162b4a440fd
Илья <ilia.volkov2016@mail.ru>
2021-12-20 21:09:31 +0300

    Урок 1. Добавил кнопку в разметку и повесил на неё clickListener в Activity.

--- app/src/main/java/ru/iliavolkov/firstappkotlin/MainActivity.kt
+++ app/src/main/java/ru/iliavolkov/firstappkotlin/MainActivity.kt
@@ -1,12 +1,18 @@
 package ru.iliavolkov.firstappkotlin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    val btn: MaterialButton = findViewById(R.id.btn)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener { Toast.makeText(this, "Нажали на $btn",Toast.LENGTH_SHORT).show() }
     }
 }
