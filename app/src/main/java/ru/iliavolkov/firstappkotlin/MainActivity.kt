839d2f676089823b5072312a9ad4e118b8347e91
Илья <ilia.volkov2016@mail.ru>
2021-12-20 22:21:58 +0300

    Урок 1. Создали object copy и вывели значения на экран

--- app/src/main/java/ru/iliavolkov/firstappkotlin/MainActivity.kt
+++ app/src/main/java/ru/iliavolkov/firstappkotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.iliavolkov.firstappkotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
@@ -11,17 +12,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btn: MaterialButton
     private lateinit var text1: TextView
     private lateinit var text2: TextView
+    private lateinit var text3: TextView
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val dataClass = DataClass("Илья", 18)
+        var copy = dataClass.copy(age = 16)
         init()
 
         btn.setOnClickListener {
             Toast.makeText(this, "Нажали на кнопку = $btn",Toast.LENGTH_SHORT).show()
             text1.text = dataClass.name
             text2.text = dataClass.age.toString()
+            text3.text = "${copy.name} ${copy.age}"
         }
     }
 
@@ -29,5 +34,6 @@ class MainActivity : AppCompatActivity() {
         btn = findViewById(R.id.btn)
         text1 = findViewById(R.id.text1)
         text2 = findViewById(R.id.text2)
+        text3 = findViewById(R.id.text3)
     }
 }
